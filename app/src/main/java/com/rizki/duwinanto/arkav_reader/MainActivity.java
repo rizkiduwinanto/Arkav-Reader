9e0c51fb6e85a8cbaedd5656cdaf4f479763481b
rizkiduwinanto <rizkiduwinanto@gmail.com>
2018-01-30 12:48:45 +0700

    Main dulu deh

--- app/src/main/java/com/rizki/duwinanto/arkav_reader/MainActivity.java
+++ app/src/main/java/com/rizki/duwinanto/arkav_reader/MainActivity.java
@@ -3,18 +3,40 @@ package com.rizki.duwinanto.arkav_reader;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+    private ZXingScannerView mScannerView;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mScannerView = new ZXingScannerView (this);
+        setContentView(mScannerView);
     }
 
-    public void goToScan(View view) {
-        Intent Scan = new Intent(this, ScanActivity.class);
-        startActivity(Scan);
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+    @Override
+    public void handleResult(Result rawResult){
+        Log.v(TAG, rawResult.getText());
+        Log.v(TAG, rawResult.getBarcodeFormat().toString());
+
+        mScannerView.resumeCameraPreview(this);
     }
 }
